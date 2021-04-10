c5bba8c0ae5734f96d3cdbe25497eb63b0169cc9
AsYouWantTeam <rejvenerin@gmail.com>
2021-04-10 12:44:00 +0300

    first commit

---
+++ src/main/java/ru/ayw/kinko/KinkoApplication.java
@@ -0,0 +1,13 @@
+package ru.ayw.kinko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinkoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ayw/kinko/KinkoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ayw.kinko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
